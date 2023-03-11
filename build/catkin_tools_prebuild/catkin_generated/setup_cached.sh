74f43409f4900b97988d3af6107e530b18044534
Maxwell Zheng <zhengmaxwell@gmail.com>
2023-03-11 18:29:37 -0500

    renew init

---
+++ build/catkin_tools_prebuild/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/rob498/catkin_ws/devel/.private/catkin_tools_prebuild:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/opt/ros/melodic/lib'
+export PWD='/home/rob498/catkin_ws/build/catkin_tools_prebuild'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/rob498/catkin_ws/devel/.private/catkin_tools_prebuild/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH='/home/rob498/catkin_ws/build/catkin_tools_prebuild:/home/rob498/catkin_ws/src/VIO:/opt/ros/melodic/share'
---
+++ build/px4_realsense_bridge/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/rob498/catkin_ws/devel/.private/px4_realsense_bridge:$CMAKE_PREFIX_PATH"
+export PWD='/home/rob498/catkin_ws/build/px4_realsense_bridge'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/rob498/catkin_ws/devel/.private/px4_realsense_bridge/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/rob498/catkin_ws/src/VIO:$ROS_PACKAGE_PATH"
